e0bfc153faa6fc1451ae4c4a376e54cd1bed810d
Aditya <75035219+pegvin@users.noreply.github.com>
2023-05-23 19:29:45 +0530

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/adityam/Documents/cs-libpng
+# Build directory: /home/adityam/Documents/cs-libpng/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
